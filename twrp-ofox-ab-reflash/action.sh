205058f8d112f584c3385726cd1a5f8c70085bff
Mr. Doge <78793304+mrdoge0@users.noreply.github.com>
2026-05-20 18:46:58 +0300

    Delete twrp-ofox-ab-reflash directory

--- twrp-ofox-ab-reflash/action.sh
+++
@@ -1,6 +0,0 @@
-#!/system/bin/sh
-TMPDIR="/data/adb/doge-hypersimple-twrpsurvive-tmp-$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 20 | head -n 1)"
-
-# Create tmpdir
-echo "Creating temporary dir ${TMPDIR}"
-mkdir "${TMPDIR}"
